0a6118ac08bb3a4f1125c7183e6eb4430c7fa789
YueLone <laryr16945@gmail.com>
2024-12-28 19:05:22 +0800

    feat(Instagram)Component:add Container component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import Header from "components/Header";
+import Container from "components/Container";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
-        {children}
+        <Container>{children}</Container>
       </body>
     </html>
   );
