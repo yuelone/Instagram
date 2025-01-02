2724dd42465ae4bd78c1f793b648836a50588834
YueLone <laryr16945@gmail.com>
2025-01-02 23:56:15 +0800

    feat(Instagram)-utils: create readDataWithTimeout function

--- app/api/suggested/route.ts
+++ app/api/suggested/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
-import fs from "fs";
+
 import path from "path";
+
+import readDataWithTimeout from "utils/readDataWithTimeout"
 
 interface Suggested {
   id: string;
@@ -17,29 +19,10 @@ interface Data {
 
 const dbPath: string = path.join(process.cwd(), "data", "suggested.json");
 
-const readData = (): Data => {
-  const fileContent = fs.readFileSync(dbPath, "utf8");
-  return JSON.parse(fileContent) as Data;
-};
-
 export const GET = async () => {
   try {
-    const data = await new Promise<Data>((resolve, reject) => {
-      const timeout = setTimeout(() => {
-        reject(new Error("Request timed out"));
-      }, 1000);
-
-      try {
-        const result = readData();
-        resolve(result);
-      } catch (error) {
-        reject(error);
-      } finally {
-        clearTimeout(timeout);
-      }
-    });
-
-    return NextResponse.json(data.suggested);
+    const response = (await readDataWithTimeout<Data>(dbPath, 1000)).suggested
+    return NextResponse.json(response);
   } catch (error: unknown) {
     const message = error instanceof Error ? error.message : "Unknown error";
     return NextResponse.json({ message }, { status: 500 });
