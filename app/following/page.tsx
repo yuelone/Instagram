922f5605f24fc45b55830ab8bec93e9497b65e85
YueLone <laryr16945@gmail.com>
2025-02-08 14:41:43 +0800

    feat(Instagram)-page:Complete connection to following API

--- app/following/page.tsx
+++ app/following/page.tsx
@@ -1,18 +1,62 @@
+"use client";
+
+import React, { useEffect } from "react";
+
+import { useAppDispatch } from "hook/customReduxHook";
+
+import { setFollowingUsers } from "slices/following";
+
+import { useFollowingData } from "selector/following";
+
 import Container from "components/Container";
+import Loading from "components/Loading";
 import UserList from "components/UserList";
 
+import { useGetFollowingQuery } from "services/following";
+
 const Following: React.FC = () => {
+  const {
+    isLoading: serviceFollowingLoading,
+    data: serviceFollowingData,
+    refetch,
+  } = useGetFollowingQuery();
+
+  const dispatch = useAppDispatch();
+
+  const followingData = useFollowingData();
+
+  useEffect(() => {
+    refetch();
+  }, []);
+
+  useEffect(() => {
+    if (!serviceFollowingLoading && serviceFollowingData) {
+      dispatch(setFollowingUsers(serviceFollowingData));
+    }
+  }, [serviceFollowingLoading, serviceFollowingData]);
+
   return (
     <Container>
       <p className="my-8 font-bold text-2xl px-4 box-border">Following</p>
-      <div className="shadow-md mt-8 mx-2 box-border">
-        <UserList
-          account="taeyeon_ss"
-          avatar="/images/avatar/taeyeon.jpg"
-          showFollow
-          isFollowing
-        />
-      </div>
+      {serviceFollowingLoading && !serviceFollowingData && (
+        <div className="flex justify-center w-full">
+          <Loading />
+        </div>
+      )}
+      {!serviceFollowingLoading &&
+        followingData &&
+        followingData.map((item) => (
+          <UserList
+            key={item.id}
+            id={item.id}
+            type="following"
+            account={item.account}
+            verify={item.verify}
+            avatar={item.avatar}
+            showFollow
+            isFollowing={item.isFollowing}
+          />
+        ))}
     </Container>
   );
 };
